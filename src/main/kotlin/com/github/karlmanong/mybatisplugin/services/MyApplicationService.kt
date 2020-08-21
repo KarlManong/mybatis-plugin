a970ad9eb3befff5e0daaaee071716eb5436f304
GitHub Action <action@github.com>
2020-08-21 02:21:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/karlmanong/mybatisplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/karlmanong/mybatisplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.karlmanong.mybatisplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.karlmanong.mybatisplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/karlmanong/mybatisplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/karlmanong/mybatisplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.karlmanong.mybatisplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.karlmanong.mybatisplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
